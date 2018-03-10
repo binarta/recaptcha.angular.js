d15d9e7ac6dfdec102cda5d90192d24a49712244
wro <wouter@websters.be>
2018-03-10 14:23:03 +0100

    install default recaptcha public key into config

--- src/recaptcha.js
+++ src/recaptcha.js
@@ -1,1 +1,5 @@
-angular.module('bin.recaptcha', ['vcRecaptcha']);
+angular.module('bin.recaptcha', ['vcRecaptcha'])
+    .config(['configProvider', function(configProvider) {
+        configProvider.add({recaptchaPublicKey: '6LeHfkUUAAAAAMPb3ijamARFhQyh6e16ezW4S2t5'});
+    }])
+;
